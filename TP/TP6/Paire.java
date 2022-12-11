2dccb98c3c1035a2318a39daedc444e5216668c6
Marcus <70955436+spc-ezual@users.noreply.github.com>
2022-12-11 17:51:12 +0100

    Update Paire.java

--- TP/TP6/Paire.java
+++ TP/TP6/Paire.java
@@ -1,31 +1,18 @@
+public class Paire<a1,a2>{
+    a1 Key;
+    a2 Value;
 
-public class Paire<T1, T2> {
-    protected T1 Key;
-    protected T2 Value;
-
-    public T1 getKey() {
-        return this.Key;
-    }
-
-    public void setKey(T1 Key) {
-        this.Key = Key;
-    }
-
-    public T2 getValue() {
-        return this.Value;
-    }
-
-    public void setValue(T2 Value) {
-        this.Value = Value;
-    }
-    public Paire(T1 k,T2 V){
+    public Paire(a1 k,a2 j){
         Key=k;
-        Value=V;
+        Value =j;
     }
 
+    public a1 getKey(){return Key;}
+    public a2 getValue(){return Value;}
+    public void setKey(a1 c){Key=c;}
+    public void setValue(a2 v){Value=v;}
     @Override
     public String toString() {
         return "{"+Key+","+Value+"}";
     }
-
 }
