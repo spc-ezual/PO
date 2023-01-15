0081d53508f746589b35231b985f0e260fdd9979
Marcus <70955436+spc-ezual@users.noreply.github.com>
2023-01-15 20:06:54 +0100

    Delete tt.java

--- Projet/tt.java
+++
@@ -1,7 +0,0 @@
-public class tt {
-    public static void main(String[] args) {
-        ArrayList<Double> coef = new ArrayList<Double>();
-        coef.add(null);
-        System.out.println(Collections.max(coef).doubleValue());
-    }
-}
